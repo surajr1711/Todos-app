1dc863cd4735f1b25af7302c629ae61f3a997a34
Suraj Radhakrishnan <surajr1711@gmail.com>
2025-01-25 17:56:41 +0530

    Optimistic add & toggle todos provides instant user feedback

--- src/features/todos/api/apiSlice.ts
+++ src/features/todos/api/apiSlice.ts
@@ -26,6 +26,7 @@ export const todosApiSlice = createApi({
 			// transformResponse: (res: Todo[]) => res.sort((a, b) => b.createdDate - a.createdDate),
 			providesTags: ["Todos"],
 		}),
+
 		addTodo: builder.mutation<TodoDocument, Todo>({
 			query: (newTodo) => ({
 				url: "/todos",
@@ -33,7 +34,43 @@ export const todosApiSlice = createApi({
 				body: newTodo,
 			}),
 			invalidatesTags: ["Todos"],
+			// Optimistic update to add a todo
+			async onQueryStarted(todo, { dispatch, queryFulfilled }) {
+				const tempId = crypto.randomUUID();
+				const tempCreatedAt = new Date();
+				const tempUpdatedAt = tempCreatedAt;
+
+				const optimisticTodo: TodoDocument = {
+					...todo,
+					_id: tempId,
+					createdAt: tempCreatedAt,
+					updatedAt: tempUpdatedAt,
+				};
+
+				const patchResult = dispatch(
+					todosApiSlice.util.updateQueryData("getTodos", undefined, (draft) => {
+						draft.push(optimisticTodo);
+					})
+				);
+
+				try {
+					const { data: serverTodo } = await queryFulfilled;
+					dispatch(
+						todosApiSlice.util.updateQueryData("getTodos", undefined, (draft) => {
+							// find the index of optimisitic todo that we just added to the cache
+							const index = draft.findIndex((t) => t._id === tempId);
+							// if its found replace the optimistic todo with the new one (containing the correct mongodb_id and timestamps) that returned from the server.
+							if (index !== -1) {
+								draft[index] = serverTodo;
+							}
+						})
+					);
+				} catch {
+					patchResult.undo();
+				}
+			},
 		}),
+
 		deleteTodo: builder.mutation<Todo, string>({
 			query: (id) => ({
 				url: `/todos/${id}`,
@@ -41,6 +78,7 @@ export const todosApiSlice = createApi({
 			}),
 			invalidatesTags: ["Todos"],
 		}),
+
 		toggleTodo: builder.mutation<TodoDocument, { _id: string; done: boolean }>({
 			query: (payload) => ({
 				url: `/todos/${payload._id}`,
@@ -48,7 +86,24 @@ export const todosApiSlice = createApi({
 				body: { done: payload.done },
 			}),
 			invalidatesTags: ["Todos"],
+			// Optimistic update to toggle todo
+			async onQueryStarted(todo, { dispatch, queryFulfilled }) {
+				const patchResult = dispatch(
+					todosApiSlice.util.updateQueryData("getTodos", undefined, (draft) => {
+						const todoToUpdate = draft.find((t) => t._id === todo._id);
+						if (todoToUpdate) {
+							todoToUpdate.done = !todoToUpdate.done;
+						}
+					})
+				);
+				try {
+					await queryFulfilled;
+				} catch {
+					patchResult.undo();
+				}
+			},
 		}),
+
 		editTodo: builder.mutation<TodoDocument, { _id: string; text: string }>({
 			query: (payload) => ({
 				url: `/todos/${payload._id}`,
